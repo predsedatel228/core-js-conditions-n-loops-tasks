7a909331453a26c4ad2143a3d38cbd90be1e55e4
Egor <predsedatel228@yandex.ru>
2024-01-27 17:25:23 +0300

    add solution for getIndexOf

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -219,8 +219,14 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let answer = -1;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      answer = i;
+    }
+  }
+  return answer;
 }
 
 /**
